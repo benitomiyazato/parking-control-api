8ab20c070ad7ce4dbddeb5d8d61af73c83e77bb3
bemiyaz <benito.miya172@gmail.com>
2022-09-14 15:22:06 -0300

    UserDetailsServiceImpl

---
+++ src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.api.parkingcontrol.configs.security;
+
+import com.api.parkingcontrol.models.UserModel;
+import com.api.parkingcontrol.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserModel> fetchedUser = userRepository.findByUsername(username);
+        if(fetchedUser.isEmpty())
+            throw new UsernameNotFoundException("User not found with username: " + username);
+
+        return fetchedUser.get();
+    }
+}
--- src/main/java/com/api/parkingcontrol/repositories/UserRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import com.api.parkingcontrol.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
+    Optional<UserModel> findByUsername(String username);
 }
