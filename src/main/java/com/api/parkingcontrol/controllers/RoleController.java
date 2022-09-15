80d00620b08f66c95bd6984b689f9007f5c48e05
bemiyaz <benito.miya172@gmail.com>
2022-09-15 16:31:24 -0300

    RoleController PreAuthorization all endpoints

--- src/main/java/com/api/parkingcontrol/controllers/RoleController.java
+++ src/main/java/com/api/parkingcontrol/controllers/RoleController.java
@@ -7,6 +7,7 @@ import com.api.parkingcontrol.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,10 @@ public class RoleController {
     @Autowired
     private RoleService roleService;
 
+    // TODO:
+    // Duplicate Roles in DB
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<Object> saveRole(@RequestBody @Valid RoleDto roleDto) {
         RoleModel roleModel = new RoleModel();
